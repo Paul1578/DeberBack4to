e1918d41e92ff93060dd2a78a446cdd73a10d5be
Paul Carrillo <lpa.carrillo@yavirac.edu.ec>
2025-04-06 19:31:44 -0500

    modificaciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit,ChangeDetectionStrategy } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 import { ApiCostumerService } from './api-costumer.service';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatCardModule} from '@angular/material/card';
+import {MatChipsModule} from '@angular/material/chips'; 
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HttpClientModule, CommonModule],
+  imports: [ HttpClientModule, CommonModule, MatCardModule, MatChipsModule, MatProgressBarModule, MatToolbarModule,MatButtonModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
